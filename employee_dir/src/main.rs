79d7e67536e872a943ce9d396a35744e4a407233
Ruchira Datta <Ruchira.Datta@gmail.com>
2019-07-09 23:18:59 -0700

    Added Directory command to list all employees in all departments.

--- employee_dir/src/main.rs
+++ employee_dir/src/main.rs
@@ -6,6 +6,7 @@ use std::collections::HashMap;
 
 fn main() {
     let add_re = Regex::new("Add (.*) to (.*)").expect("Couldn't compile add_re");
+    let directory_re = Regex::new("Directory").expect("Couldn't compile directory_re");
     let list_re = Regex::new("List (.*)").expect("Couldn't compile list_re");
     let mut command = String::new();
     let mut department_dirs: HashMap<String, Vec<String>> = HashMap::new();
@@ -27,6 +28,15 @@ fn main() {
                     }
                 } else {
                     println!("Unknown department {}", &dept);
+                }
+            }
+        } else if directory_re.is_match(&command) {
+            println!("Employees by Department");
+            for (dept, dir) in department_dirs.iter_mut() {
+                println!("  {}", &dept);
+                dir.sort(); 
+                for employee in dir.iter() {
+                    println!("    {}", employee);
                 }
             }
         } else if add_re.is_match(&command) {
